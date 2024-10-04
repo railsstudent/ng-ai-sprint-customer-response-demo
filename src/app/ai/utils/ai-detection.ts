2ab9bd9b53e529f451360ab6080985a5c30af4cf
Connie Leung <cleung334@gmail.com>
2024-10-04 21:04:59 +0800

    refactor: better checking on ai assistant

--- src/app/ai/utils/ai-detection.ts
+++ src/app/ai/utils/ai-detection.ts
@@ -2,5 +2,9 @@ import { inject } from '@angular/core';
 import { AI_ASSISTANT_TOKEN } from '../constants/core.constant';
 
 export function isPromptAPISupported(): boolean {
+   console.log('ai' in globalThis);
+   const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+   const version = raw ? parseInt(raw[2], 10) : 0;
+   console.log(version);
    return !!inject(AI_ASSISTANT_TOKEN);
 }
